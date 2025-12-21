9e4114e5c3998c175a5f374d6765ebf73ea52888
Andrew <mggamii7@gmail.com>
2025-12-21 09:43:59 +0200

    Add labs 02-06

---
+++ Lab06/app/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="blog_index"),
+    path("articles/", views.article_list, name="article_list"),
+    path("articles/<int:pk>/", views.article_detail, name="article_detail"),
+    path("categories/", views.category_list, name="category_list"),
+]
